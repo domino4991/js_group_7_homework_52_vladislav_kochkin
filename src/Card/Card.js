c4e8c3a1a4f841632c74f44b1ee164ed08770f44
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-20 17:55:21 +0600

    Rewrote a little code for Card.js

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Card = (props) => {
+    let cardClassName = props.selected ? `card rank-${props.rank} ${props.suit} selected` : `card rank-${props.rank} ${props.suit}`;
     let suit;
     switch(props.suit) {
         case "hearts":
@@ -20,7 +21,7 @@ const Card = (props) => {
             break;
     }
     return (
-        <div className={"card rank-" + props.rank + " " + props.suit}>
+        <div className={cardClassName} onClick={props.selectedCard}>
             <span className="rank">{props.rank.toUpperCase()}</span>
             <span className="suit">{suit}</span>
         </div>
