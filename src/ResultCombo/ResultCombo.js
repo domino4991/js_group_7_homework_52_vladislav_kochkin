b8179996462d987046b13358fcc30cf9e544ab35
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-20 15:38:36 +0600

    Added ResultCombo.js component

---
+++ src/ResultCombo/ResultCombo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResultCombo = (props) => {
+    return (
+        <div className="result-combo">
+            <p>{props.result}</p>
+        </div>
+    );
+}
+
+export default ResultCombo;
